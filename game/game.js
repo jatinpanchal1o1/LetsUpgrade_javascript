43380a2c901a3d4deb2e7b4d836867a2691574b1
Jatin Panchal <50742576+jatinpanchal1o1@users.noreply.github.com>
2022-05-14 13:25:29 +0530

    game update health

--- game/game.js
+++ game/game.js
@@ -1,27 +1,26 @@
+////--------------Creating HTML Content-------------------
+
 const div1 = document.createElement("div")
 div1.style.textAlign = "center"
+
+//-------------Creating "Shooting Game" Heading-----------
 const h1 = document.createElement("h1")
 h1.innerText = "Shooting Game"
 div1.append(h1)
 const div2 = document.createElement("div")
 div1.append(div2)
+
 document.body.append(div1)
 
 
-var player1 = prompt("Type Player1 Name: ")||"player1"
-var player2 = prompt("Type Player2 Name: ")||"player2"
-var currentPlayer = player1 || "player";
-var player1Score = 0;
-var player1Health = 100
-var player2Health = 100
-var player2Score = 0;
+//-----------Create "Score" Heading----------------------- 
 
 const heading = document.createElement("h3")
 heading.innerText = "Score Screen"
 div1.append(heading)
 
-///
 
+//-------------Create Button For Start Game----------------
 
 const button = document.createElement("button")
 button.setAttribute("id", "btn")
@@ -29,28 +28,74 @@ button.innerText = "Start"
 div1.append(button)
 
 
-
+///-------------Create Function and variables For Game--------------------
 const btn = document.getElementById("btn")
-
-
 btn.addEventListener("click", Play)
 
+
+var player1 = prompt("Type Player1 Name: ")||"player1"
+var player2 = prompt("Type Player2 Name: ")||"player2"
+var currentPlayer = player1 || "player";
+var player1Score = 0;
+var player2Score = 0;
+
+
+//--------------Function which call in "addEventListener"---------------
 function Play(){
-               
+
+////progress 1st player health
+const div3 = document.createElement("div")
+div3.style.display="flex"
+div3.style.justifyContent="space-around"
+
+const div4 = document.createElement("div")
+div4.style.display="flex"
+div4.style.justifyContent="space-around"
+
+////player1 heading which user enter 
+const heading1 = document.createElement("h3")
+heading1.innerText = `${player1}`
+div4.append(heading1)
+////player2 heading which user enter  
+const heading2 = document.createElement("h3")
+heading2.innerText = `${player2}`
+div4.append(heading2)
+
+///progress of 2nd player health
+const progress1 = document.createElement("progress")
+progress1.setAttribute("id", "health1")
+progress1.setAttribute("value", "100")
+progress1.setAttribute("max", "100")
+div3.append(progress1)
+
+///progress of 2nd player health
+const progress2 = document.createElement("progress")
+progress2.setAttribute("id", "health2")
+progress2.setAttribute("value", "100")
+progress2.setAttribute("max", "100")
+div3.append(progress2)
+div2.append(div4)
+div2.append(div3)
+
+var player1Health = document.getElementById("health1")
+var player2Health = document.getElementById("health2")
+            
+//----------Use "setInterval" For loop---------
           var x = 0
           var interval = setInterval(()=>{
             let randomNumber = Math.floor(Math.random() * 100)
-            
+            //use random value always to change player 
+            //incressing there score and decrease other player health
             if(randomNumber <= 60){
               if(currentPlayer == player1){
                 player1Score++
-                player2Health--
+                player2Health.value -= 30
               }
               else{
                 player2Score++
-                player1Health--
+                player1Health.value -= 30
               }
-
+             //print values on screen
               heading.innerText = currentPlayer + " shoot! \n" +
               player1 + ": " + player1Score + "\n" +
               player2 + ": " + player2Score + "\n" 
@@ -60,7 +105,7 @@ function Play(){
                 player1 + ": " + player1Score + "\n" +
                 player2 + ": " + player2Score + "\n" 
 
-
+               //swap players every time in else
                 if (currentPlayer == player1) {
                     currentPlayer = player2;
                   } else {
@@ -69,7 +114,7 @@ function Play(){
 
             }
 
-              
+ //---------Condition For Break interval loop------------------             
             if(player1Score < 5 && player2Score < 5 && ++x===5){
 
               window.clearInterval(interval);
@@ -95,22 +140,3 @@ function Play(){
   
           }, 3000)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
